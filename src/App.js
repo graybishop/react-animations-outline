c7b63b2bdd89316424e89e2810b2d9299f01391b
Jake Swackhammer <j.100101@gmail.com>
2021-12-08 15:47:11 -0500

    set-up an example using react-transition-group

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import CSSTransitionGroupExample from './components/CSSTransitionGroupExample.js';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto flex flex-col justify-center items-center gap-4 py-8' >
+      <h1 className='text-3xl'>Testing happens below</h1>
+      <CSSTransitionGroupExample/>
     </div>
   );
 }
