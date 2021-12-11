5c7b6da08e642789d930e636cf7d543f377fcd83
Jake Swackhammer <j.100101@gmail.com>
2021-12-11 13:13:56 -0500

    set up framer-motion

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import CSSTransitionGroupExample from './components/CSSTransitionGroupExample.js';
+import FramerMotionExample from './components/FramerMotionExample.js';
 import GSAPExample from './components/GSAPExample.js';
 
 
 function App() {
 
   return (
-    <div className='container mx-auto flex flex-col justify-center items-center gap-4 py-8' >
+    <div className='container mx-auto flex flex-col justify-center items-center gap-4 py-8 bg-gray-600' >
       <h1 className='text-3xl'>Testing happens below</h1>
       <CSSTransitionGroupExample/>
       <GSAPExample/>
+      <FramerMotionExample/>
     </div>
   );
 }
